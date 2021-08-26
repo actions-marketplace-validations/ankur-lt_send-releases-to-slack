3032627f8dcbbd000abdbe0099ef479dd922f978
Ankur Narkhede <ankurn@lambdatest.com>
2021-08-26 17:07:23 +0530

    [MLE-5505] Added safechecks for accessing object properties

--- dist/index.js
+++ dist/index.js
@@ -1534,8 +1534,8 @@ try {
   console.log('Changelog Body received::', body);
 
   if (typeof body === 'string') {
-    console.log('Parsing the stringified body to JSON')
-    body = JSON.parse(body)
+    console.log('Parsing the stringified body to JSON');
+    body = JSON.parse(body);
   }
 
   if (_.isEmpty(body) && !Array.isArray(body)) {
@@ -1545,12 +1545,14 @@ try {
 
   // Get first release of the body for normal release, else the last one for reverted release
   if (revertedRelease === 'true' || revertedRelease === true) {
-    body = body[body.length - 1]
+    body = body[body.length - 1];
+    console.log('Getting the last release as revertedRelease::', revertedRelease);
+  } else {
+    body = body[0];
+    console.log('Getting the first release as revertedRelease::', revertedRelease);
   }
-  else {
-    body = body[0]
-  }
-  console.log('Current release changelog parsed as::', JSON.stringify(body))
+
+  console.log('Current release changelog parsed as::', JSON.stringify(body));
 
   // initialise skeleton of slack message body
   let slackMessageBody = {
@@ -1582,13 +1584,14 @@ try {
   let slackChangelog = '```\n';
 
   // Assign header for message
-  _.set(slackMessageBody, 'blocks[0].text.text', `${repoName} ${_.get(body, 'title')}`);
+  _.set(slackMessageBody, 'blocks[0].text.text',
+      `${repoName} ${_.get(body, 'title')}`);
 
   // Assign changelog title
-  let changelogTitle = 'Changelog'
+  let changelogTitle = 'Changelog';
 
   if (body.href) {
-    changelogTitle = `*<${body.href}|Changelog>*`
+    changelogTitle = `*<${body.href}|Changelog>*`;
   }
 
   _.set(slackMessageBody, 'blocks[1].text.text', changelogTitle);
@@ -1605,18 +1608,19 @@ try {
 
   // Generate changelog from fixes
   _.isArray(body.fixes) && _.forEach(body.fixes, (log) => {
-    slackChangelog += `- <${log.href}|${_.get(log, 'subject') || _.get(log, 'message')}>\n`;
+    slackChangelog += `- <${log.href}|${_.get(log, 'subject') ||
+    _.get(log, 'message')}>\n`;
   });
 
   // Add closing code snippets to the body
-  slackChangelog += '```'
+  slackChangelog += '```';
 
   // Assign generated changelog to the body
   _.set(slackMessageBody, 'blocks[2].text.text', slackChangelog);
 
   // Send slack alert
   // Channel has been configured in the respective slack app
-  console.log('Sending slack message as::', JSON.stringify(slackMessageBody))
+  console.log('Sending slack message as::', JSON.stringify(slackMessageBody));
   slack.alert(slackMessageBody);
 
   slack.onError = function(err) {
